86d99e7c682933c29d7cd9602d4a07a4e49d821d
Prabhnoor Sandhu <prabhcode@gmail.com>
2025-06-20 15:41:30 -0700

    Update message to test GitHub Actions trigger

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🔥 Updated message: Flask running on Kubernetes!"
+    return "🚀 Hello world from GitHub Actions + DockerHub + ArgoCD!"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
